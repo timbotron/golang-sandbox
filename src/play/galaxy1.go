e328841c3e20db16900fa5f1b4f582bff5e0971c
Tim Habersack <timh@darkhorse.com>
2015-05-15 13:02:30 -0700

    progress

---
+++ src/play/galaxy1.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+import "math/rand"
+import "encoding/json"
+
+type Pin struct {
+	X int
+	Y int
+	T int
+}
+
+var seed = int64(5185441982)
+
+func born(x int, y int) bool {
+	fmt.Println("running..")
+	var starCount = int(float32(x) * float32(y) * 0.05)
+	fmt.Printf("Star Count: %d\n", starCount)
+	rand.Seed(seed)
+	var starChart = make([]Pin, starCount)
+	for ii := 0; ii < starCount; ii++ {
+		var newPin = genPin(&starChart, x, y)
+		//var newPin = Pin{randInt(-x, x), randInt(-y, y), 1}
+		starChart[ii] = newPin
+	}
+	fmt.Printf("%v\n", starChart)
+	fmt.Printf("%d\n", len(starChart))
+	starJSON, err := json.Marshal(starChart)
+	fmt.Printf("Errors: %v\n", err)
+	fmt.Printf("Star JSON: %s\n", starJSON)
+	return true
+}
+func genPin(pins *[]Pin,x int,y int) Pin { // TODO: figure out slice pointers
+	var newPin = Pin{randInt(-x, x), randInt(-y, y), 1}
+	if isPinUsed(pins,newPin) {
+		newPin = genPin(pins, x, y)
+	}
+	
+	return newPin
+}
+
+
+func isPinUsed(pins *[]Pin,pin Pin) bool {
+	for ii := 0; ii < len(pins); ii++ {
+		if pins[ii].X == pin.X && pins[ii].Y == pin.Y {
+			return false
+		}
+	}
+	return true
+}
+
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+func main() {
+
+	born(15, 15)
+}
--- src/play/test1.go
+++ src/play/test1.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("playtime")
+	a := [5]int{1, 2, 3, 4, 5}
+	pa := &a
+	fmt.Println("orig:",a)
+	fmt.Println("pointer:",pa)
+	fmt.Println("orig_len:",len(a))
+	fmt.Println("pointer_len:",len(pa))
+	calcLen := lenfunc(pa)
+	fmt.Println("lenfunc:",calcLen)
+}
+
+func lenfunc(thing *[5]int) int {
+	return len(thing)
 }
