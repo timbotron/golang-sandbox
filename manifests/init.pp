2e24d621eab8844869a4b763e0b773dd40cf2d45
Tim Habersack <timh@darkhorse.com>
2015-05-15 12:42:03 -0700

    first commit of my funtimes golang box

---
+++ manifests/init.pp
@@ -0,0 +1,16 @@
+stage { "prepare":
+  before => Stage["main"],
+}
+
+class setup {
+  exec { "update-aptitude":
+    command => "/usr/bin/apt-get update -y",
+  }
+}
+
+class {
+  "setup":
+    stage => prepare;
+  "golang":
+  version => '1.4.2';
+}
