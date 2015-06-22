d70c38c50f7fcca8ac5362cba8116b4d3cae09fd
Tim Habersack <timh@darkhorse.com>
2015-06-22 12:50:30 -0700

    added more star types

--- src/play/gen_star.go
+++ src/play/gen_star.go
@@ -22,7 +22,38 @@ func gen_map() map[string]st {
 
 	r := make(map[string]st)
 
-	r["g"] = st{cls: "a",
+	
+	r["b"] = st{cls: "b",
+				min_t: 100,
+				max_t: 300,
+				color: "deep blue white",
+				min_m: 2.1,
+				max_m: 16,
+				min_r: 1.8,
+				max_r: 6.6,
+				min_l: 25,
+				max_l: 30000}
+	r["a"] = st{cls: "a",
+				min_t: 75,
+				max_t: 100,
+				color: "blue white",
+				min_m: 1.4,
+				max_m: 2.1,
+				min_r: 1.4,
+				max_r: 1.8,
+				min_l: 5,
+				max_l: 25}
+	r["f"] = st{cls: "f",
+				min_t: 60,
+				max_t: 75,
+				color: "white",
+				min_m: 1.04,
+				max_m: 1.4,
+				min_r: 1.15,
+				max_r: 1.4,
+				min_l: 1.5,
+				max_l: 5}
+	r["g"] = st{cls: "g",
 				min_t: 52,
 				max_t: 60,
 				color: "yellowish white",
@@ -32,6 +63,26 @@ func gen_map() map[string]st {
 				max_r: 1.15,
 				min_l: 0.6,
 				max_l: 1.5}
+	r["k"] = st{cls: "k",
+				min_t: 37,
+				max_t: 52,
+				color: "pale yellow orange",
+				min_m: 0.45,
+				max_m: 0.8,
+				min_r: 0.7,
+				max_r: 0.96,
+				min_l: 0.08,
+				max_l: 0.6}
+	r["m"] = st{cls: "m",
+				min_t: 24,
+				max_t: 37,
+				color: "light orange red",
+				min_m: 0.08,
+				max_m: 0.45,
+				min_r: 0.01,
+				max_r: 0.7,
+				min_l: 0.001,
+				max_l: 0.08}
 	return r
 }
 
